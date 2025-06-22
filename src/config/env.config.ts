aa182bbeb50158d8adb1fa2846c4fb772e5264a9
toel-kamanda34 <toelokemwa@gmail.com>
2025-06-23 00:29:30 +0300

    setting up environment variables

---
+++ src/config/env.config.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import { cleanEnv, str, port, bool } from 'envalid';
+
+dotenv.config();
+
+export const envConfig = cleanEnv(process.env, {
+  NODE_ENV: str({ choices: ['development', 'test', 'production'] }),
+  PORT: port({ default: 5000 }),
+  DB_HOST: str(),
+  DB_PORT: port({ default: 5432 }),
+  DB_USERNAME: str(),
+  DB_PASSWORD: str(),
+  DB_DATABASE: str(),
+  SECRET_KEY_ONE: str(),
+  SECRET_KEY_TWO: str(),
+  REACT_URL: str(),
+  JWT_SECRET: str()
+});
